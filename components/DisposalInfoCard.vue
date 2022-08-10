ebfa858b30b95349aaa95d23213b182c40cb8fab
Sanko <toroksanyo@gmail.com>
2022-08-10 23:11:45 +0200

    card styling

--- components/DisposalInfoCard.vue
+++ components/DisposalInfoCard.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="bg-neutral-100 p-6 rounded-lg shadow-lg mb-6">
-    <h2 class="text-2xl font-bold mb-2 text-gray-800">{{ title }}</h2>
-    <p class="text-gray-700">{{ type }}</p>
-    <font-awesome-icon icon="fa-solid fa-user-secret" />
+  <div class="bg-neutral-100 p-6 rounded-lg shadow-lg mb-6 flex flex-row">
+    <font-awesome-icon
+      class="bg-indigo-400 p-6 rounded-full mr-5 text-4xl text-white"
+      icon="fa-user-secret"
+    />
+    <div class="info-wrapper">
+      <h2 class="text-2xl font-bold mb-2 text-gray-600">{{ title }}</h2>
+      <p class="text-gray-700">{{ type }}</p>
+    </div>
   </div>
 </template>
 
