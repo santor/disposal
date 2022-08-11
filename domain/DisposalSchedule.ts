d65a88b78c71d7ea64fbc7cf9a07f8afb45709a3
Sanko <toroksanyo@gmail.com>
2022-08-11 10:33:36 +0200

    updated disposal

--- domain/DisposalSchedule.ts
+++ domain/DisposalSchedule.ts
@@ -31,21 +31,33 @@ export interface ApiResponse {
 export interface DisposalSchedule {
   date: Date;
   type: WasteType;
+  zip: number;
+  area: string;
+  region: string;
 }
 
 export class DisposalScheduleEntity implements DisposalSchedule {
   date: Date;
   type: WasteType;
+  zip: number;
+  area: string;
+  region: string;
 
   constructor(disp: DisposalSchedule) {
     this.date = disp.date;
     this.type = disp.type;
+    this.zip = disp.zip;
+    this.area = disp.area;
+    this.region = disp.region;
   }
 
   static fromResponse(response: DisposalScheduleResponse) {
     return new DisposalScheduleEntity({
       date: new Date(response.date),
       type: response.type,
+      zip: response.zip,
+      area: response.area,
+      region: response.region,
     });
   }
 }
